9514feb326da059ad64971ba1c8da655cd8c2883
AaronPecesG <aaron.peces@alu.uclm.es>
2022-05-07 11:58:36 +0200

    Archivo function_token.py creado. Este contiene algunas funciones utiles para el manejo de los tokens

---
+++ function_token.py
@@ -0,0 +1,28 @@
+from jwt import encode, decode
+from jwt import exceptions
+from os import getenv
+from datetime import datetime, timedelta
+from flask import jsonify
+
+
+def expire_date(seconds: int):
+    now = datetime.now()
+    new_date = now + timedelta(seconds)
+
+def write_token(data:dict):
+    token = encode(payload={**data,"exp": expire_date(5)}, key=getenv("SECRET"),algorithm="HS256")
+    return token.encode("UF-8)")
+
+def validate_token(token, output = False):
+    try:
+        if output:
+            return decode(token,key=getenv("SECRET"),algorithms="HS256")
+        decode(token,key=getenv("SECRET"),algorithms="HS256")
+    except exceptions.DecodeError:
+        response = jsonify({"message": "Invalid Token"})
+        response.status_code = 401
+        return response
+    except exceptions.ExpiredSignatureError:
+        response = jsonify({"message": "Token Expired"})
+        response.status_code = 401
+        return response
